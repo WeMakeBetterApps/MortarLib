1a41619e7f88b799b81c65c28b6adff214d36682
Scott Pierce <spierce7@gmail.com>
2015-02-02 17:51:55 -0500

    Added some util methods.

--- lib/src/main/java/mortar/lib/util/MortarUtil.java
+++ lib/src/main/java/mortar/lib/util/MortarUtil.java
@@ -1,8 +1,12 @@
 package mortar.lib.util;
 
+import android.app.Activity;
+import android.app.Application;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.view.View;
 
+import flow.Layout;
 import flow.Layouts;
 import mortar.Blueprint;
 import mortar.Mortar;
@@ -11,10 +15,23 @@ import mortar.MortarScope;
 public class MortarUtil {
 
   public static View createScreen(Context context, Blueprint screen) {
-    MortarScope myScope = Mortar.getScope(context);
-    MortarScope newChildScope = myScope.requireChild(screen);
+    MortarScope scope = Mortar.getScope(context);
+    MortarScope newChildScope = scope.requireChild(screen);
+
     Context childContext = newChildScope.createContext(context);
     return Layouts.createView(childContext, screen);
+  }
+
+  public static int getLayoutId(Blueprint screen) {
+    Class<? extends Blueprint> screenType = screen.getClass();
+    Layout layoutAnnotation = screenType.getAnnotation(Layout.class);
+    if (layoutAnnotation == null) {
+      throw new IllegalArgumentException(
+          String.format("@%s annotation not found on class %s", Layout.class.getSimpleName(),
+              screenType.getName()));
+    }
+
+    return layoutAnnotation.value();
   }
 
   public static void destroyScreen(View view) {
@@ -25,6 +42,21 @@ public class MortarUtil {
     MortarScope parentScope = Mortar.getScope(parentContext);
     MortarScope oldChildScope = Mortar.getScope(view.getContext());
     parentScope.destroyChild(oldChildScope);
+  }
+
+  public static Activity getActivity(Context context) {
+    Context currentContext = context;
+    while (currentContext instanceof ContextWrapper) {
+      currentContext = ((ContextWrapper) currentContext).getBaseContext();
+
+      if (currentContext instanceof Activity) {
+        return (Activity) currentContext;
+      } else if (currentContext instanceof Application) {
+        return null;
+      }
+    }
+
+    return null;
   }
 
 }
